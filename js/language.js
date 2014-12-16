bea42de616a61c07c6b180a1e632aee578be3ef8
utsabn34 <utsabn34@gmail.com>
2014-12-16 21:36:23 +0545

    tabbing structure finished

--- js/language.js
+++ js/language.js
@@ -12,35 +12,37 @@ function Country()
 	var flagbtn = (document.getElementById("flagbtn"));
 	var flag = (document.getElementById("flag"));
 	var that = this;
-	this.flags = [];
+	this.flags =[];
 	this.ul;
 	this.parent =cntry;
-	this.out = optionbar;
+	this.optionbar = optionbar;
 	this.countryName = [];
-	this.form = cntryform;
-	this.flagElement = flag;
-	this.button = flagbtn;
-	this.selected = this.form.children[0];
+	this.cntryform = cntryform;
+	this.choose = this.cntryform.children[0];
+	this.flag = flag;
+	this.flagbtn = flagbtn;
 	
 	
 	this.list = function()
 	{
-		for(var i=0;i<that.selected.childElementCount;i++)
+		for(var i=0;i<that.choose.childElementCount;i++)
 		{
-			var option = that.selected.children[i];
+			var option = that.choose.children[i];
 			var src = option.getAttribute("pic");
 			that.flags.push(src);
-			var src = option.value;
-			that.countryName.push(src);
+			var source = option.value;
+			that.countryName.push(source);
 		}
-		console.log(that.flags);
-		console.log(that.countryName);
+		
 	}();
 	
-	this.createMenu = function()
+	this.menu = function()
 	{
 		var ul = document.createElement("ul");
-		that.out.appendChild(ul);
+		ul.style.float="none";
+		ul.style.display= "none";
+		ul.style.width="80px";
+		that.optionbar.appendChild(ul);
 		that.ul = ul;
 		
 		
@@ -48,29 +50,39 @@ function Country()
 		for(var i=0;i<that.flags.length;i++)
 		{
 			var li = document.createElement("li");
+			li.style.float="left";
+			li.style.marginTop="2px";
+			li.style.height="18px";
+			li.style.listStyle="none";
 			ul.appendChild(li);
-			
-			var img = document.createElement("img");
-			li.appendChild(img);
-			img.setAttribute("src",that.flags[i]);
 			
 			var span = document.createElement("span");
 			li.appendChild(span);
 			span.innerHTML = that.countryName[i];
 			
+			var img = document.createElement("img");
+			img.style.cursor="pointer";
+			img.style.height="15px";
+			img.style.float="left";
+			li.appendChild(img);
+			img.setAttribute("src",that.flags[i]);
+			
+			
+			
 			li.onclick = (function(i,ul)
 			{
 				return function()
 				{
-					that.form.value = that.countryName[i];
-					that.flagElement.setAttribute("src",that.flags[i]);
-					ul.style.display = "none";
+					that.choose.children[i].value = that.countryName[i];
+					that.flag.setAttribute("src",that.flags[i]);
+					that.ul.style.display = "none";
 				}
 			})(i,ul);
 			
-			that.button.onclick = function()
+			that.flagbtn.onclick = function()
 			{
 				that.ul.style.display = "block";
+				
 			}
 		}
 	}();
